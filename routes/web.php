2ff4b4b8c67a32f56458c8797a0e50a1957dbb6b
Lotrando <miroslav.klika@seznam.cz>
2023-04-03 06:56:25 +0200

    update template and routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index', [
         'title'     => 'Index',
-        'uzivatel'  => 'Klika Miroslav',
-        'funkce'    => 'Vývojář, webmaster',
     ]);
 })->name('index');
